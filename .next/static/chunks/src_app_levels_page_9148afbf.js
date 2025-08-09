facd4451c4573275077f0a1f1ff18eff1540df28
Ainur Fadhil <89878403+PudilN@users.noreply.github.com>
2025-08-09 17:13:08 +0700

    Add .next/static/chunks/src_app_levels_page_9148afbf.js 1754734388560

---
+++ .next/static/chunks/src_app_levels_page_9148afbf.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_1f602bc6._.js"
+],
+    source: "dynamic"
+});
