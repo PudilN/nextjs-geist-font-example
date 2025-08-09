8283085ab31540b1f297da086dbfac15eff4d194
Ainur Fadhil <89878403+PudilN@users.noreply.github.com>
2025-08-09 17:13:30 +0700

    Add .next/static/chunks/src_app_dashboard_page_9148afbf.js 1754734410848

---
+++ .next/static/chunks/src_app_dashboard_page_9148afbf.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_719209b6._.js"
+],
+    source: "dynamic"
+});
